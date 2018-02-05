ebbe346c0e0c6fe0e4ec611a2bfaa1e4d7b8a3f8
ZheSun88 <zhe@vaadin.com>
2018-02-05 11:03:50 +0200

    Using new webjars
    renaming repository id;
    add logging dependency;
    remove HtmlImport in component demo file.

--- src/test/java/com/vaadin/flow/component/formlayout/demo/FormLayoutView.java
+++ src/test/java/com/vaadin/flow/component/formlayout/demo/FormLayoutView.java
@@ -24,7 +24,6 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.formlayout.FormLayout.FormItem;
 import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
@@ -33,8 +32,6 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.Binder;
-import com.vaadin.flow.data.binder.Binder.Binding;
 import com.vaadin.flow.data.binder.BinderValidationStatus;
 import com.vaadin.flow.data.binder.BindingValidationStatus;
 import com.vaadin.flow.data.validator.EmailValidator;
@@ -50,11 +47,6 @@ import com.vaadin.flow.router.Route;
  * @author Vaadin Ltd
  */
 @Route("vaadin-form-layout")
-@HtmlImport("frontend://bower_components/vaadin-text-field/theme/lumo/vaadin-text-field.html")
-@HtmlImport("frontend://bower_components/vaadin-button/theme/lumo/vaadin-button.html")
-@HtmlImport("frontend://bower_components/vaadin-form-layout/theme/lumo/vaadin-form-layout.html")
-@HtmlImport("frontend://bower_components/vaadin-date-picker/theme/lumo/vaadin-date-picker.html")
-@HtmlImport("frontend://bower_components/vaadin-checkbox/theme/lumo/vaadin-checkbox.html")
 public class FormLayoutView extends DemoView {
 
     @Override
