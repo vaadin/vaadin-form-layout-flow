0fbe48adf1d99585d816ff90d8fad1683c8fec63
No <svipal@tuta.io>
2019-03-28 15:35:40 +0100

    fix for objects can't be resolved

--- vaadin-form-layout-flow/src/main/java/com/vaadin/flow/component/formlayout/FormLayout.java
+++ vaadin-form-layout-flow/src/main/java/com/vaadin/flow/component/formlayout/FormLayout.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.vaadin.flow.component.ClickNotifier;
